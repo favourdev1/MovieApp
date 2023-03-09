669d0e604041a3bea9fc6e02a488debb87f1af75
Favourdev <favourapps17@gmail.com>
2023-03-09 13:30:11 +0100

    fixed backdrop text issues

--- resources/views/containerUi/home-top.blade.php
+++ resources/views/containerUi/home-top.blade.php
@@ -4,7 +4,7 @@
     @endphp @include('layouts.navbar')
 
     <div class="h-full pb-12 mt-36 flex flex-col justify-end mt-auto">
-        <p class="text-white rounded-full px-5 py-2 backdrop-blur-sm backdrop-brightness-50 bg-white/30 w-max  text-sm md:text-md ">
+        <p class="text-white rounded-full px-5 py-2 backdrop-blur-sm backdrop-brightness-10 bg-white/30 w-max  text-sm md:text-md mb-6">
             New Movie
         </p>
         <div class="text-white w-full md:w-2/3 xl:w-2/4">
