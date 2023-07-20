85f579ed7e133f4952f796b880cd35ca0a27f116
Favour Okechukwu <favourapps17@gmail.com>
2023-07-20 18:26:56 +0100

    pulled from auth branch

--- resources/views/components/ui/search-bar.blade.php
+++ resources/views/components/ui/search-bar.blade.php
@@ -1,8 +1,8 @@
 <form action="#" method="get"
-    {{ $attributes->merge(['class' => 'rounded-full bg-neutral-950 text-sm text-neutral-500 py-4 px-4 flex items-center']) }}>
+    {{ $attributes->merge(['class' => 'rounded-full bg-neutral-950 text-sm text-neutral-500 py-1 px-4 flex items-center']) }}>
 
     <x-icon.search-icon></x-icon.search-icon>
-    <input type="text" name="search" class="  border-transparent bg-transparent  focus:outline-0 w-full ml-2 font-light"
+    <input type="text" name="search" class=" text-sm border-transparent bg-transparent  focus:outline-0 w-full ml-2 font-light"
         placeholder="Search movies" value="{{ $slot }}" id="search">
     <x-icon.filter-icon></x-icon.filter-icon>
         
